a27d6eb4c0f369062e3290aae274356fa3361631
kimkiyoung <rlar0313@naver.com>
2023-02-08 17:22:53 +0900

    수정중

--- script/main.js
+++ script/main.js
@@ -1,20 +1,19 @@
 //DOM select(선택)
 
-const opener = document.querySelector(".opener"); 
-const closer = document.querySelector("#closer");//기호
+//DOM Select(선택)
+const opener = document.querySelector(".opener"); //기호
+const closer = document.querySelector("#closer");  //기호
 const menu = document.querySelector(".menu");
 
-opener.addEventListener("click", function () {
-     menu.classList.remove("hide")
+//event 추가 : .addEventListener() vs Event 제거: .removeEventListener
+opener.addEventListener("click", function () { //클릭 : 마우스 클릭 이벤트
+  //menu.classList.remove("hide");//숨겨져있던 .menu 나타내기.
+  menu.style.display = "block";
+  console.log("hide 클래스를 제거했습니다.");
+});
 
-    console.log("사이드 메뉴를 호출했습니다..");
-      });
-
-      //addEventListener 이벤트 추가
-    //event제거 -? removed..
-
-    closer.addEventListener("click", function () {
-      menu.classList.add("hide")
- 
-      console.log("사이드 메뉴를 추가했습니다..");
-       });
+closer.addEventListener("click", function () { //클릭 : 마우스 클릭 이벤트
+  //menu.classList.add("hide"); 실제로는 문제 없지만 테스트 환경 고려. 
+  menu.style.display = "none";
+  console.log("hide 클래스를 추가했습니다.");
+});
